4053552664f38f17da0be18770e6787fcb71f619
Riccardo <riccardo.romeo46@gmail.com>
2024-11-03 16:30:16 +0100

    implementata l'ottimizzazione  in SMT

--- SMTMOD/SMT/SMT_solver.py
+++ SMTMOD/SMT/SMT_solver.py
@@ -3,3 +3,18 @@ from pysmt.typing import INT
 from SMT.utils import *
 import time as t
 
+
+
+class SMTsolver:
+    def __init__(self, input_data, output_path, strategy, timeout=300, symmetry_breaking="False", fair_division="no_fair", model="z3"):
+        self.input_data = input_data
+        self.timeout = timeout
+        self.solver = Solver()
+        self.model = model
+        self.output_path = output_path
+        self.strategy = strategy
+        self.symmetry_breaking = symmetry_breaking
+        self.fair_division = fair_division
+
+    def set_solver(self):
+        self.solver = Solver(name=self.model)
--- SMTMOD/SMT/SMT_utils.py
+++ SMTMOD/SMT/SMT_utils.py
@@ -1,6 +1,7 @@
 from itertools import combinations
 from pysmt.shortcuts import Symbol, And, Or, Not
 from pysmt.typing import BOOL
+import numpy as np
 
 
 # NAIVE PAIRWISE ENCODING
@@ -26,3 +27,19 @@ def at_most_one_he(bool_vars, name):
 
 def exactly_one_he(bool_vars, name):
     return And(at_most_one_he(bool_vars, name), at_least_one_he(bool_vars))
+
+
+
+def set_lower_bound(D):
+    # If the courier must carry at least one item then we use as lower bound the longest distance base ->item -> base
+    last_row = D[-1]
+    last_column = D[:,-1]
+    value1 = last_column[np.argmax(last_row)] + max(last_row)
+    value2 = last_row[np.argmax(last_column)] + max(last_column)
+    lb = max(value1, value2)
+    return lb 
+
+def set_upper_bound(D, n):
+    # As upper bound we use the maximum distance that one courier would travel if he carries all items
+    ub = D[n,0] + np.sum(D[0: n-1, 1: n].diagonal()) + D[n-1, n]    
+    return ub
